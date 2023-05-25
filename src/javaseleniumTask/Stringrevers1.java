b08a9181d34b2388dd3557b10f45d093ce734927
Akshaya1224 <akshayaofficial124@gmail.com>
2023-05-25 09:46:06 +0530

    gitbranch

--- src/javaseleniumTask/Stringrevers1.java
+++ src/javaseleniumTask/Stringrevers1.java
@@ -5,7 +5,7 @@ package javaseleniumTask;
 
 public class Stringrevers1 {
 public static void main(String []args) {
-	String input="akshaya";
+	String input="aks";
 	StringBuffer sbr=new StringBuffer(input); //conversion from string pbject to string buffer
 	sbr.reverse();//reverse the string
 	System.out.println("Enter the reversed string:"+ sbr);
